cbab7d60b8c1b97c385a712b48c2be258dadc4d9
Fabrizio Montesi <famontesi@gmail.com>
2018-08-08 14:22:01 +0200

    Function initialiser for jquery-jolie

--- web/lib/jquery-jolie.js
+++ web/lib/jquery-jolie.js
@@ -1,23 +1,22 @@
-// Global Jolie object
-var Jolie = {
-	// Calls an operation at the originating server using JSON
-	call: function( params ) {
-		if ( typeof params.service === 'undefined' ) {
-			params.url = '/' + params.operation;
-		} else {
-			params.url = '/!' + params.service + '!/' + params.operation;
+(function() {
+	// Global Jolie object
+	window.Jolie = {
+		// Calls an operation at the originating server using JSON
+		call: function( params ) {
+			if ( typeof params.service === 'undefined' ) {
+				params.url = '/' + params.operation;
+			} else {
+				params.url = '/!' + params.service + '!/' + params.operation;
+			}
+			params.dataType = 'json';
+			params.contentType = 'application/json';
+			params.data = JSON.stringify( params.data );
+
+			if ( typeof params.method === 'undefined' ) {
+				params.method = 'POST';
+			}
+
+			$.ajax( params );
 		}
-		params.dataType = 'json';
-		params.contentType = 'application/json';
-		params.data = JSON.stringify( params.data );
-
-		if ( typeof params.method === 'undefined' ) {
-			params.method = 'POST';
-		}
-
-		$.ajax( params );
-	}
-};
-
-// Make Jolie global
-window.Jolie = Jolie;
+	};
+}());
